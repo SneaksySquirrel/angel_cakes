be1c04298146d90928d58617bafa009659f49fe1
trevorpirie <drizztpirie@hotmail.com>
2019-06-19 21:01:37 +1200

    Check in missing files

---
+++ Assets/Scripts/CollectableCake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(AudioSource))]
+public class CollectableCake : MonoBehaviour
+{
+
+    public AudioClip collectableSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            //Increase number of cakes player has eaten
+            GetComponent<AudioSource>().PlayOneShot(collectableSound);
+            GetComponent<Renderer>().enabled = false;
+
+            //Destroy the game object after the sound has completed
+            Destroy(gameObject, collectableSound.length);
+
+            //Destroy this instance of the script to prevent it being called again.
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
 
-    private Rigidbody2D rigidbody;
+    private new Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        var x = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var y = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        //Set the position of our character throught the RigidBody2D component (since we are using physics)
+        rigidbody.MovePosition(new Vector2(x, y));
+
     }
 }
