958d54220ea0f2a0d685aee620647e4dbdd83bd0
trevorpirie <drizztpirie@hotmail.com>
2019-06-19 18:38:29 +1200

    Check in foldes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
